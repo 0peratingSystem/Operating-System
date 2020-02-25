ddab0ad76697650f21b18a47be19513d45b1f854
yibyeongyong <51373248+yibyeongyong@users.noreply.github.com>
2020-02-25 16:30:42 +0900

    Fork: Fork from Operating-System, merged with my codes My enviroment is CLion with Cmake.

---
+++ cmake-build-debug/CMakeFiles/MINT64.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/usr/CLionProjects/Operating-System/MINT64/00.BootLoader/disk_read.c" "/usr/CLionProjects/Operating-System/cmake-build-debug/CMakeFiles/MINT64.dir/MINT64/00.BootLoader/disk_read.c.o"
+  "/usr/CLionProjects/Operating-System/MINT64/main.c" "/usr/CLionProjects/Operating-System/cmake-build-debug/CMakeFiles/MINT64.dir/MINT64/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
