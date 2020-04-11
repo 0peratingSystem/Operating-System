9609a65a5e6880261dc9552d08c4fff6b45ba8fe
zzbyeongyong <zzbyeongyong@gmail.com>
2020-04-11 15:35:19 +0900

    Added script conditional statement for QEMU environment compatibility.

--- qemu.sh
+++ qemu.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Made some minor changes to the script by zzbyeongyong
 
-qemu -L . -m 64 -fda ./Disk.img -localtime -M pc
+if qemu -L . -m 64 -fda ./Disk.img -localtime -M pc ; then
+	echo "Operating Succeeded"
+else qemu-system-x86_64 -L . -m 64 -fda ./Disk.img -M pc
+# if-else for qemu compatibility.
+fi
